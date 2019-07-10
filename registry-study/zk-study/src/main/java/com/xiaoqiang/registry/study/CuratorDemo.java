eb51e7a89e31e8ca9f1f2d616eaed66061b5ef5f
75616 <756164814@qq.com>
2019-07-10 23:02:59 +0800

    zk 学习

---
+++ registry-study/zk-study/src/main/java/com/xiaoqiang/registry/study/CuratorDemo.java
@@ -0,0 +1,38 @@
+package com.xiaoqiang.registry.study;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+
+public class CuratorDemo {
+
+    public static void main(String[] args) throws Exception {
+        CuratorFramework curatorFramework = ZKUtils.getCuratorFramework();
+
+        curatorFramework.start();
+
+        createData(curatorFramework);
+        getDate(curatorFramework);
+        updateData(curatorFramework);
+        deleteData(curatorFramework);
+    }
+
+    private static void createData(CuratorFramework framework) throws Exception {
+        framework.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath("/data/curdTest", "Create test".getBytes());
+    }
+
+    private static void getDate(CuratorFramework framework) throws Exception {
+        System.out.println(new String(framework.getData().forPath("/data/createTest")));
+    }
+
+    private static void updateData(CuratorFramework framework) throws Exception {
+        framework.setData().forPath("/data/createTest", "Updated test".getBytes());
+    }
+
+    private static void deleteData(CuratorFramework framework) throws Exception {
+        Stat stat = new Stat();
+        framework.getData().storingStatIn(stat).forPath("/data/curdTest");
+
+        framework.delete().withVersion(stat.getVersion()).forPath("/data/curdTest");
+    }
+}
--- registry-study/zk-study/src/main/java/com/xiaoqiang/registry/study/ZKUtils.java
+++ registry-study/zk-study/src/main/java/com/xiaoqiang/registry/study/ZKUtils.java
@@ -1,14 +1,29 @@
 package com.xiaoqiang.registry.study;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
 
 public class ZKUtils {
-    public static String ADDRESS = "192.168.3.10:2181";
+    public static final String ADDRESS = "192.168.3.10:2181";
+    public static final String L_CLUSTER_URL = "localhost:2181,localhost:2182,localhost:2183";
+    public static final String V_CLUSTER_URL="192.168.3.10:2181,192.168.3.10:2182,192.168.3.10:2183";
+    public static final int SESSION_TIMEOUT = 10000;
+    public static final int BASE_SLEEP_TIME = 100;
+    public static final int MAX_RETRIES = 3;
+
+
 
     public static ZooKeeper getClient(Watcher watcher) throws IOException {
-        return new ZooKeeper(ADDRESS, 200000, watcher);
+        return new ZooKeeper(ADDRESS, SESSION_TIMEOUT, watcher);
+    }
+
+    public static CuratorFramework getCuratorFramework() {
+        return CuratorFrameworkFactory.builder().connectString(V_CLUSTER_URL).sessionTimeoutMs(SESSION_TIMEOUT)
+                .retryPolicy(new ExponentialBackoffRetry(BASE_SLEEP_TIME, MAX_RETRIES)).build();
     }
 }
